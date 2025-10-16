e79f276584e154c46166d12f410511425dc747e3
barbaradomith <barbaradomithsouza@gmail.com>
2025-10-15 22:06:39 -0300

    feat: Implementada tag BrowserRouter

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./components/navbar/NavBar";
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main></main>
+
+      <footer></footer>
+    </>
+  );
+}
